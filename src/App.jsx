9a66fa9ed71b35e771fa737b0df94bb830cb3988
Maroua Ezzaki Kadmiri <maroua.ezzaki@bootcamp-upgrade.com>
2025-02-07 16:04:22 +0100

    created first button using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ButtonIcon from './components/ButtonIcon'
 
 import './App.css'
 
 export default function App() {
   
+    return (
+        <ButtonIcon/>
+    )
 }
---
+++ src/components/ButtonIcon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ButtonIcon ()  {
+    return (
+
+        <button>Hola a todos</button>
+
+    )
+}
